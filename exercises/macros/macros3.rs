0979b101f121febd95fe7ad871819b5e5978fbc0
robin.wu <robin.wu@nio.com>
2024-10-13 16:19:23 +0800

    完成macros

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -8,13 +8,13 @@
 // I AM NOT DONE
 
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
 }
-
 fn main() {
     my_macro!();
 }
