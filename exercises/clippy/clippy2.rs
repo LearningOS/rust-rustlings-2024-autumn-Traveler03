8b01dd6c0562a14e957ee8345aa0abd9f71ee202
robin.wu <robin.wu@nio.com>
2024-10-13 16:38:04 +0800

    完成clippy

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,12 +3,12 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
+    // 这里不能用while let会陷入无限循环
+    if let Some(x) = option {
         res += x;
     }
     println!("{}", res);
